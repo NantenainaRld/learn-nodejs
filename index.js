e68b691841a0bcdb136433b1e8eacd35fc13f19c
NantenainaRld <nantenainarld@gmail.com>
2026-04-02 16:04:24 +0300

    docs: 02 - JSON

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
 // console.log(chain(5).add(2).divide(3).multiply(4).done()); // ((5 + 2) / 2) * 4 = 9.3333
 // =================================
 
-// =================================
+// ================================
 // # 03 - Module
 
 //-- Chalk
@@ -39,3 +39,32 @@
 // console.log(chalk.bgRed("Red background"));
 // console.log(chalk.green.bold.bgBlue("Text green, bold, background blue"));
 // console.log(chalk.yellow.underline("Underline , text yellow"));
+
+// ================================
+// 04 - JSON
+import { read, readFileSync, writeFileSync } from "node:fs";
+
+//-- JSON to object
+// const dataObject = JSON.parse(readFileSync("./data.json", "utf-8"));
+// const { name, age } = dataObject;
+// console.log(dataObject);
+// console.log(name + " , " + age);
+
+//-- Object to json
+// const myObject = {
+//   name: "Nantenaina",
+//   age: 22,
+// };
+// console.log(JSON.stringify(myObject));
+
+//-- Create a json
+// const newData = {
+//   city: "Fianarantsoa",
+//   code: 301,
+// };
+// writeFileSync("./newData.json", JSON.stringify(newData));
+
+//-- Update json
+// const data = JSON.parse(readFileSync("./data.json", "utf-8"));
+// data.name = "Edouardo";
+// writeFileSync("./data.json", JSON.stringify(data));
