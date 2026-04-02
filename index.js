68169f19bd97c9e36e6b4ffe6a4dc0f8e9289709
NantenainaRld <nantenainarld@gmail.com>
2026-04-02 06:24:04 +0300

    docs: 0 - First script

---
+++ index.js
@@ -0,0 +1,15 @@
+// ================================
+// # 01 - First Script
+
+// console.log("Hello World");
+
+// FIlesystem
+// const fs = require("fs");
+
+//-- writeFileSync
+// fs.writeFileSync("test01.txt", "This is the test for the writeFileSync");
+
+//-- readFileSync
+// const fileContent = fs.readFileSync("test01.txt", "utf8");
+// console.log(fileContent);
+// =================================
