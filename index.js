8645145cfc9e01a6beab0372902963842f298e8d
NantenainaRld <nantenainarld@gmail.com>
2026-04-02 11:05:22 +0300

    docs: 02 - IMport/export module

--- index.js
+++ index.js
@@ -13,3 +13,19 @@
 // const fileContent = fs.readFileSync("test01.txt", "utf8");
 // console.log(fileContent);
 // =================================
+
+// =================================
+// # 02 - Import/export module
+
+//-- CommonJS
+// const mod = require("./module");
+// console.log(mod.function00() + " " + mod.function01());
+
+//-- ES6 module
+// import { function00, function01 } from "./module.js";
+// console.log(function00(), function01());
+
+//-- Math module
+import { chain } from "mathjs";
+
+console.log(chain(5).add(2).divide(3).multiply(4).done()); // ((5 + 2) / 2) * 4 = 9.3333
---
+++ module.js
@@ -0,0 +1,15 @@
+function function00() {
+  return "Function 00";
+}
+function function01() {
+  return "Function 01";
+}
+
+//-- CommonJS
+// module.exports = {
+//   function00,
+//   function01,
+// };
+
+//-- ES6 module
+export { function00, function01 };
